c8bd685a6d1daed181bb75c4d39361c792fa5700
Sin <346490349@qq.com>
2017-06-06 17:46:36 +0800

    First commit.

---
+++ app/src/main/java/sin/ubusadministrator/UbusAdministratorActivity.java
@@ -0,0 +1,13 @@
+package sin.ubusadministrator;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UbusAdministratorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ubus_administrator_layout);
+    }
+}
